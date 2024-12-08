6e6bfb6c914f297f68ec8b0e8f498589308683f5
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-12-09 01:22:52 +0200

    feat: create a question shuffling function, add a file with questions and answers

---
+++ hangman/scripts/questions.js
@@ -0,0 +1,82 @@
+export const questions = [
+  {
+    hint: 'Who is the lead singer of the band Queen?',
+    answer: 'Mercury',
+  },
+  {
+    hint: 'Which method adds an element to the end of an array?',
+    answer: 'Push',
+  },
+  {
+    hint: 'Who is known as the "King of Pop"?',
+    answer: 'Jackson',
+  },
+  {
+    hint: 'What does "AI" stand for in technology?',
+    answer: 'Artificial',
+  },
+  {
+    hint: 'What is the name of Harry Potter’s owl?',
+    answer: 'Hedwig',
+  },
+  {
+    hint: 'Which empire built the Colosseum in Rome?',
+    answer: 'Roman',
+  },
+  {
+    hint: "What is the name of Voldemort's snake?",
+    answer: 'Nagini',
+  },
+  {
+    hint: 'Who is considered the founder of Microsoft?',
+    answer: 'Gates',
+  },
+  {
+    hint: 'What keyword is used to create a constant variable in JavaScript?',
+    answer: 'Const',
+  },
+  {
+    hint: 'What is the traditional Sicilian term for "Godfather"?',
+    answer: 'Don',
+  },
+  {
+    hint: 'What is the name of the sport played at Wimbledon?',
+    answer: 'Tennis',
+  },
+  {
+    hint: 'Which Italian dish is made of layers of pasta, meat, and cheese?',
+    answer: 'Lasagna',
+  },
+  {
+    hint: 'What is the smallest country in the world?',
+    answer: 'Vatican',
+  },
+  {
+    hint: 'What mythical creature is half lion and half eagle?',
+    answer: 'Griffin',
+  },
+  {
+    hint: 'What is the fictional language spoken by the Elves in The Lord of the Rings?',
+    answer: 'Elvish',
+  },
+  {
+    hint: 'What is the tallest type of tree in the world?',
+    answer: 'Redwood',
+  },
+  {
+    hint: 'What is the name of the galaxy we live in?',
+    answer: 'MilkyWay',
+  },
+  {
+    hint: 'In "The Collector", what does Frederick collect?',
+    answer: 'Butterflies',
+  },
+  {
+    hint: 'What do you call a song without any instruments?',
+    answer: 'Acapella',
+  },
+  {
+    hint: 'What does "HTTP" stand for?',
+    answer: 'Protocol',
+  },
+];
